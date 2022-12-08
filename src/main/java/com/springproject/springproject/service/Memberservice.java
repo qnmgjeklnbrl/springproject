feac79ec8fe7ae9530763cbfef6c683fd40a7ca1
qnmgjeklnbrl <rns0721@naver.com>
2022-12-08 17:40:56 +0900

    12/08

--- src/main/java/com/springproject/springproject/service/Memberservice.java
+++ src/main/java/com/springproject/springproject/service/Memberservice.java
@@ -3,20 +3,13 @@ package com.springproject.springproject.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+
 import org.springframework.stereotype.Service;
 
 @Service
-public class Memberservice  implements UserDetailsService ,OAuth2UserService<OAuth2UserRequest,OAuth2User>{
+public class Memberservice  implements UserDetailsService{
 
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
